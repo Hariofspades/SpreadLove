057612c1f3da3584fd12c9903d0c3dbc81a9b615
Hari <hariutd@gmail.com>
2018-05-23 08:10:45 +0530

    adding model to scene

--- app/src/main/java/com/hariofspades/spreadlove/MainActivity.kt
+++ app/src/main/java/com/hariofspades/spreadlove/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.hariofspades.spreadlove
 
 import android.graphics.Point
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import com.google.ar.core.Anchor
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -31,7 +37,50 @@ class MainActivity : AppCompatActivity() {
             fragment.onUpdate(it)
             onUpdate()
         }
-        fab.setOnClickListener {}
+        fab.setOnClickListener {
+            addObject(Uri.parse("Heart.sfb"))
+        }
+    }
+
+    private fun addObject(parse: Uri) {
+        val frame = fragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(fragment, hit.createAnchor(), parse)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun placeObject(fragment: ArFragment, createAnchor: Anchor, model: Uri) {
+        val rederableFuture = ModelRenderable.builder()
+                .setSource(fragment.context, model)
+                .build()
+                .thenAccept {
+                    addNodeToScene(fragment, createAnchor, it)
+                }
+                .exceptionally {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setMessage(it.message)
+                            .setTitle("error!")
+                    val dialog = builder.create()
+                    dialog.show()
+                    return@exceptionally null
+                }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(createAnchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
     }
 
     private fun onUpdate() {
