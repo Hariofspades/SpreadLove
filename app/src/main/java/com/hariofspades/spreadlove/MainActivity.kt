55bcb557b9738679d99a159e7b5def39909ceecc
Hari <hariutd@gmail.com>
2018-05-23 07:49:09 +0530

    tracking planes and hittests

--- app/src/main/java/com/hariofspades/spreadlove/MainActivity.kt
+++ app/src/main/java/com/hariofspades/spreadlove/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.hariofspades.spreadlove
 
+import android.graphics.Point
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.ux.ArFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,20 +18,71 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var fragment: ArFragment
 
+    var isTracking: Boolean = false
+    var isHitting: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
-
+        fragment.arSceneView.scene.setOnUpdateListener {
+            fragment.onUpdate(it)
+            onUpdate()
+        }
         fab.setOnClickListener {}
+    }
+
+    private fun onUpdate() {
+        val trackingChanged = updateTracking()
+        val contentView = findViewById<View>(android.R.id.content)
+        if (trackingChanged) {
+            contentView.invalidate()
+        }
+        if (isTracking) {
+            val hittestChanged = updateHitTest()
+            if (hittestChanged) {
+                contentView.invalidate()
+            }
+        }
+    }
+
+    private fun updateTracking(): Boolean {
+        val frame = fragment.arSceneView.arFrame
+        val wasTracking = isTracking
+        isTracking = frame.camera.trackingState == TrackingState.TRACKING
+        return isTracking != wasTracking
+    }
+
+    private fun updateHitTest(): Boolean {
+        val frame = fragment.arSceneView.arFrame
+        val point: Point = getScreenCenter()
+        var hits : List<HitResult> = listOf()
+        var wasHitting = isHitting
+        isHitting = false
+        if (frame != null) {
+            hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    isHitting = true
+                    break
+                }
+            }
+        }
+        return wasHitting != isHitting
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
